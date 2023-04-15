5bb96b9ba17a047454f501f206139ebbf5fa9f9c
martapivko <111050342+martapivko@users.noreply.github.com>
2023-04-15 21:46:58 +0200

    Add files via upload

---
+++ Augmentation.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow import ImageDataGenerator
+
+def ToAugmantate(train_augmentate,test_augmentate):
+    if train_augmentate == True:
+        train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+    rotation_range=0,
+    zoom_range=0.0,
+    horizontal_flip= True ,
+    vertical_flip = False,
+    rescale=None
+)
+    else:
+        test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+    rotation_range=0,
+    zoom_range=0.0,
+    horizontal_flip= True ,
+    vertical_flip = False,
+    rescale=None
+)
